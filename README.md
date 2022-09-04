aee9c6d3d6c829877a83bac8514e0fa83aaea8ed
shraddha-kaparwan <shraddha.kaparwan@gmail.com>
2022-09-03 19:53:36 -0600

    Updating index.html, README.md, app.js and styles.css.
    Updating index.html, README.md, app.js and styles.css.

--- README.md
+++ README.md
@@ -1,13 +1,34 @@
 # Landing Page Project
 
+
 ## Table of Contents
 
-* [Instructions](#instructions)
+* [Description](#description)
+* [ProjectStructure](#project_structure)
+* [Usage](#usage)
+* [Dependencies](#dependencies)
 
-## Instructions
+## Description
+- This is a multi-section landing page with a dynamic navigation menu. 
+- When user scrolls to a section on landing page, the navigation item for that section is highlighted.
+- When User clicks on a navigation menu section in the navbar, it scrolls to that section on the landing page.
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+## ProjectStructure
+    css
+    - styles.css    
+    index.html
+    js
+    - app.js
+    README.md
 
-To get started, open `js/app.js` and start building out the app's functionality
+## Usage
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+- Dynamic navigation menu is genrated based on the number of sections on landing page.
+- Scroll up and down to highlight the corresponding navigation item in the nav bar.
+- Click on the section in the nav bar to scroll to any section on the landing page.
+
+
+## Dependencies
+- There are no external dependencies.
+- index.html requires app.js and styless.css to function properly
+- app.js depends on the contents of index.html and styles.css
--- js/app.js
+++ js/app.js
@@ -23,12 +23,22 @@
  * 
 */
 
+const sections = document.querySelectorAll('[data-nav]');
+const navbar = document.querySelector('#navbar__list');
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+/*
+    activate nav when scrolled to a section
+*/
+function activate(section) {
+    document.addEventListener("scroll", function () {
+        activateNav(section);
+    });
+}
 
 
 
@@ -39,12 +49,51 @@
 */
 
 // build the nav
+/*
+    buildNav function creates an li and anchor elements
+    finds text and id of given section element and assigns them to anchor element
+    appends anchor to li and li to navbar 
+*/
+function buildNav(section) {
+    const li = document.createElement('li');
+    const anchor = document.createElement('a');
+    anchor.textContent = section.dataset.nav;
+    anchor.classList.add("menu__link");
+    anchor.href = `#${section.id}`;
+    li.appendChild(anchor);
+    navbar.append(li);
+}
 
 
 // Add class 'active' to section when near top of viewport
+/*
+    when section is in viewport add class active
+    when section is not in viewport remove class active
+*/
+function activateNav(section) {
+    const rect = section.getBoundingClientRect();
+    const section_nav = document.querySelector(`[href="#${section.id}"]`);
+    if (rect.top <= 200 && rect.bottom >= 200) {
+        section_nav.classList.add("active");
+    } else {
+        section_nav.classList.remove("active");
+    }
+    
+}
 
 
 // Scroll to anchor ID using scrollTO event
+function scrollToSection(section_nav) {
+    section_nav.addEventListener("click", (e) => {
+        e.preventDefault();
+        scrollBy({
+            top: document
+                .querySelector(e.target.getAttribute("href"))
+                .getBoundingClientRect().top,
+            behavior: "smooth",
+          });
+    });
+}
 
 
 /**
@@ -54,9 +103,14 @@
 */
 
 // Build menu 
-
-// Scroll to section on link click
+sections.forEach(buildNav)
 
 // Set sections as active
+sections.forEach(activate)
+
+// Scroll to section on link click
+document.querySelectorAll('.menu__link').forEach(scrollToSection)
+
+
 
 
